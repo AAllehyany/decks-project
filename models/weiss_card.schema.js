f527d6190e31eb5a8ec5c97afef6e40475a24601
Ahmad Allehyany <aallehyany@gmail.com>
2020-10-30 03:30:43 +0300

    min and max items

--- models/weiss_card.schema.js
+++ models/weiss_card.schema.js
@@ -17,9 +17,12 @@ const searchQuerySchema = Joi.object({
     card_type: Joi.number().min(0).max(2),
     color: Joi.number().min(1).max(4),
     soul: Joi.number().min(-1),
-    cost: Joi.number().min(-1),
-    level: Joi.number().min(-1),
-    power: Joi.number().min(-1),
+    min_cost: Joi.number().min(-1),
+    max_cost: Joi.number().min(-1),
+    min_level: Joi.number().min(-1),
+    max_level: Joi.number().min(-1),
+    min_power: Joi.number().min(-1),
+    max_power: Joi.number().min(-1),
     text: Joi.string(),
 })
 
