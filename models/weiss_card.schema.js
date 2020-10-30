7df21444f3dcc1eff18ee7ff5878f10223ccde87
Ahmad Allehyany <aallehyany@gmail.com>
2020-10-30 03:29:11 +0300

    search query

--- models/weiss_card.schema.js
+++ models/weiss_card.schema.js
@@ -12,7 +12,19 @@ const createCardSchmea = Joi.object({
     text: Joi.string().default(''),
 });
 
+const searchQuerySchema = Joi.object({
+    name: Joi.string(),
+    card_type: Joi.number().min(0).max(2),
+    color: Joi.number().min(1).max(4),
+    soul: Joi.number().min(-1),
+    cost: Joi.number().min(-1),
+    level: Joi.number().min(-1),
+    power: Joi.number().min(-1),
+    text: Joi.string(),
+})
+
 
 module.exports = {
     createCardSchmea,
+    searchQuerySchema,
 }
