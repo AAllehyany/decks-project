fa627296d7fc61229797f937b7f0b2aaba974e13
Ahmad Allehyany <aallehyany@gmail.com>
2020-10-31 17:36:15 +0300

    user and token routes

--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,19 @@ import bodyparser from 'koa-bodyparser';
 import json from 'koa-json';
 import db from './database';
 import cardRoutes from './routes/weiss-cards.route';
+import userRoutes from './routes/user.routes';
+import tokenRoutes from './routes/token.routes';
 
 const app = new Koa();
-const router = new Router();
 const weissCardRoutes = cardRoutes(db);
-
-router.get('/', async(ctx,next) => {
-    ctx.body = {message: "Hello there!"};
-    await next();
-});
-
+const usersRoutes = userRoutes(db);
+const tokenRoute = tokenRoutes(db);
 
 app.use(json());
 app.use(bodyparser());
-app.use(router.routes()).use(router.allowedMethods());
+app.use(usersRoutes.routes()).use(usersRoutes.allowedMethods());
 app.use(weissCardRoutes.routes()).use(weissCardRoutes.allowedMethods());
-
+app.use(tokenRoute.routes()).use(tokenRoute.allowedMethods());
 app.listen(3000, () => {
     console.log("Running.....");
 })
