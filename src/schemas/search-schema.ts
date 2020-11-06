de07602f1c218b3143513103b368c384848b2ad8
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-06 21:42:56 +0300

    search schema refactored

---
+++ src/schemas/search-schema.ts
@@ -0,0 +1,15 @@
+export interface SearchSchema {
+    name?: string,
+    card_type?: number,
+    color?: number,
+    soul?: number,
+    min_cost?: number,
+    max_cost?: number,
+    min_level?: number,
+    max_level?: number,
+    min_power?: number,
+    max_power?: number,
+    level_limit?: number,
+    game?: string,
+    skip?: number
+}
