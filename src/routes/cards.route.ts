a329188a7ff92d1eaa68d1fbc9bda9609b1f9642
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-06 21:42:48 +0300

    fixed route name and removed create path

--- src/routes/cards.route.ts
+++ src/routes/cards.route.ts
@@ -2,15 +2,14 @@ import Router from '@koa/router';
 import knex from 'knex';
 import koajwt from 'koa-jwt';
 import Joi from 'joi';
-import {searchCardsService, SearchSchema, addCardsMany} from '../services/cards.service';
-import {searchQuerySchema, weissCardSchema} from '../schemas/weiss-card.schema';
+import {searchCardsService, addCardsMany} from '../services/cards.service';
+import {weissCardSchema} from '../schemas/weiss-card.schema';
+import { SearchSchema } from '../schemas/search-schema';
 
 const setUpRoutes = (): Router =>  {
     const router = new Router({
-        prefix: '/weiss-cards',
+        prefix: '/cards',
     });
-
-    //router.use(['/create'], koajwt({secret: process.env.JWT_SECRET ?? 'secret'}));
     
     router.get('/', async (ctx) => {
         const searchQuery = ctx.query;
@@ -18,20 +17,6 @@ const setUpRoutes = (): Router =>  {
         ctx.body = result;
         ctx.status = 200;
     });
-
-    router.post('/create', async ctx => {
-        const cards = ctx.request.body;
-        const cardList = Joi.array().items(weissCardSchema);
-        await cardList.validateAsync(cards);
-        const added = await addCardsMany(cards, '1');
-        // await weissCardSchema.validateAsync(cards);
-        // const added = await addCardService(cards, 1, 1);
-        ctx.status = 200;
-        ctx.body = {
-            added
-        }
-    });
-
 
     return router;
 }
