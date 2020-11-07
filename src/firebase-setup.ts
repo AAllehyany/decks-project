07999adb0aa30e879bfe21bd62d167991f2e01d9
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-08 02:39:50 +0300

    fix setup

--- src/firebase-setup.ts
+++ src/firebase-setup.ts
@@ -1,19 +1,6 @@
 import * as admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey.json';
 
-
-const serviceAccount = {
-    "type": "service_account",
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
-    "client_email": "firebase-adminsdk-urs3b@decks-8a7c6.iam.gserviceaccount.com",
-    "client_id": "113397972746855874281",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-urs3b%40decks-8a7c6.iam.gserviceaccount.com"
-  }
-  
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     databaseURL: <string>process.env.FIREBASE_DB
