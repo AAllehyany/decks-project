27826db13b58ffc1db5fb42d66a19f380053f266
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-09 13:13:39 +0300

    validate using rules

--- src/services/deck-validation.service.ts
+++ src/services/deck-validation.service.ts
@@ -1,5 +1,40 @@
+import { DeckRule } from '../schemas/deck-rule.schema';
 import {WeissCard} from '../schemas/weiss-card.schema';
 
+
+export const weissRules: Array<DeckRule> = [
+    {targetField: {field: "name", value: 'group'}, rule: {max: 4, min: 0}},
+    {targetField: {field: "card_type", value: 2}, rule: {max: 8, min: 0}},
+    {targetField: {field: "deck", value: 'length'}, rule: {max: 50, min: 50}}
+];
+
+export const validateWithRule = (deckList: Array<any>, rules: Array<DeckRule>): Boolean => {
+
+    for(const rule of rules) {
+        const field = rule.targetField;
+        if(<string>field.value === 'group') {
+            const grouped = {} as any;
+            for(const card of deckList) {
+                if(grouped[card[field.field]] === undefined) {
+                    grouped[card[field.field]] = 0;
+                }
+        
+                grouped[card[field.field]] += 1;
+        
+                if(grouped[card[field.field]] > rule.rule.max || grouped[card[field.field]] < rule.rule.min) {
+                    return false
+                }
+            }
+        } else if(<string>field.value === 'length') {
+            if(deckList.length > rule.rule.max || deckList.length < rule.rule.min) return false; 
+        } else {
+            const filtered = deckList.reduce((p, c) => c[field.field] === field.value ? (p + 1) : p, 0);
+            if(filtered > rule.rule.max || filtered < rule.rule.min) return false;
+        }
+    }
+
+    return true;
+};
 
 export const validateWeissDeck = (deckList: Array<WeissCard>): boolean => {
     // if(deckList.length != 50) 
