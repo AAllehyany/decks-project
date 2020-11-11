d7e3c1287cb0f908557b1b7545cfb798210d71ce
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-11 23:51:40 +0300

    domain fix

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(decks-builder.com www.decks-builder.com)
+domains=(decks-builder.com)
 rsa_key_size=4096
 data_path="./proxy/certbot"
 email="aallehyany@gmail.com" # Adding a valid address is strongly recommended
