9b24e5044b4451f4e79cf86ae4ca37221f5c6214
Ahmad Allehyany <aallehyany@gmail.com>
2020-11-11 14:37:28 +0300

    fixed the middleware, used to return 404 always

--- src/middlewares/firebase-auth.ts
+++ src/middlewares/firebase-auth.ts
@@ -6,17 +6,10 @@ const middleWare = async (ctx: ParameterizedContext<DefaultState, DefaultContext
 
     const auth = ctx.req.headers.authorization;
     if(!auth) ctx.throw(401, 'Unauthorized');
-
-    try {
-        const token = auth.replace('Bearer', '').trim();
-        const decoded = await admin.auth().verifyIdToken(token);
-        ctx.state.user = decoded;
-        next();
-    } catch(err) {
-        ctx.throw(401, err)
-    }
-    
-
+    const token = auth.replace('Bearer', '').trim();
+    const decoded = await admin.auth().verifyIdToken(token);
+    ctx.state.user = decoded;
+    return next();
 }
 
 
